4cf6a211bf2f2d5b5ed9c40764ab90ffc1537fd5
piotrpiwek <piotr.piwek@aecdesign.pl>
2025-10-02 20:58:23 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,9 @@
 # Krok 1: Generujemy plik konfiguracyjny z podstawionymi zmiennymi.
 envsubst < /usr/local/tomcat/conf/Catalina/localhost/context.xml.template > /usr/local/tomcat/conf/Catalina/localhost/ROOT.xml
 
-# Krok 2: Ustawiamy opcje Javy. Zmienna JAVA_OPTS jest standardem odczytywanym przez skrypt catalina.sh.
-# To jest pewniejsza metoda niż CATALINA_OPTS.
-export JAVA_OPTS="-Dhibernate.hbm2ddl.auto=update"
+echo "--- Konfiguracja bazy danych wygenerowana pomyślnie ---"
 
-echo "--- Ustawione opcje JAVA_OPTS ---"
-echo "$JAVA_OPTS"
-echo "---------------------------------"
-
-# Krok 3: Uruchamiamy oryginalną komendę startową Tomcata.
-# Skrypt catalina.sh automatycznie użyje zmiennej JAVA_OPTS.
-exec catalina.sh run
+# Krok 2: Uruchamiamy serwer Tomcat z bezpośrednio wstrzykniętą zmienną środowiskową.
+# Polecenie 'env' uruchamia program w zmodyfikowanym środowisku.
+# To gwarantuje, że proces Javy zobaczy naszą opcję.
+exec env JAVA_OPTS="-Dhibernate.hbm2ddl.auto=update" catalina.sh run
