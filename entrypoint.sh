0bca83a3f4c4398f8dc18fd5a09a578fb9de14b5
piotrpiwek <piotr.piwek@aecdesign.pl>
2025-10-02 22:22:55 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,29 @@
 #!/bin/sh
-# entrypoint.sh
+# entrypoint.sh - ETAP 1: TWORZENIE BAZY
 
-# Krok 1: Generujemy plik konfiguracyjny z podstawionymi zmiennymi.
+# Krok 1: Generujemy plik konfiguracyjny
 envsubst < /usr/local/tomcat/conf/Catalina/localhost/context.xml.template > /usr/local/tomcat/conf/Catalina/localhost/ROOT.xml
 
 echo "--- Konfiguracja bazy danych wygenerowana pomyślnie ---"
 
-# Krok 2: Ustawiamy opcje Javy Z OGRANICZENIAMI PAMIĘCI
-# Xms - startowa ilość pamięci, Xmx - maksymalna ilość pamięci
-# To jest kluczowe dla darmowego planu Render!
-export JAVA_OPTS="-Dhibernate.hbm2ddl.auto=update -Xms256m -Xmx400m"
+# Krok 2: Ustawiamy opcje Javy na "create-drop"
+# 'create-drop' stworzy całą bazę od zera, a potem ją usunie przy zamknięciu.
+# To zmusi aplikację do stworzenia wszystkiego, czego potrzebuje.
+# Dodajemy też małe opóźnienie na końcu, żeby proces zdążył się zakończyć.
+export JAVA_OPTS="-Dhibernate.hbm2ddl.auto=create-drop -Xms256m -Xmx400m"
 
-echo "--- Ustawione opcje JAVA_OPTS ---"
-echo "$JAVA_OPTS"
-echo "---------------------------------"
+echo "--- ETAP 1: Tworzenie schematu bazy danych. To potrwa kilka minut... ---"
+echo "Ustawione JAVA_OPTS: $JAVA_OPTS"
 
-# Krok 3: Uruchamiamy oryginalną komendę startową Tomcata.
-exec catalina.sh run
+# Krok 3: Uruchamiamy serwer, pozwalamy mu stworzyć bazę, a potem go zatrzymujemy
+# Używamy 'timeout', aby serwer sam się wyłączył po 10 minutach (600s).
+# To da mu wystarczająco dużo czasu na stworzenie WSZYSTKICH tabel.
+timeout 600 catalina.sh run &
+
+# Czekamy na proces Tomcata w tle
+CATALINA_PID=$!
+wait $CATALINA_PID
+
+echo "--- ETAP 1 ZAKOŃCZONY: Schemat bazy danych powinien być gotowy. ---"
+# Zakańczamy skrypt z kodem 0 (sukces), aby Render nie zgłosił błędu.
+exit 0
